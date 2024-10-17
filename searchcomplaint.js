2b891794040a02e8ab89c542c986ff238bbe0764
brughwani <brughwani@gmail.com>
2024-10-17 07:56:52 +0530

    added more endpoints

--- searchcomplaint.js
+++ searchcomplaint.js
@@ -3,3 +3,76 @@ const Airtable = require('airtable');
 // Configure the Airtable base with your API key and base ID
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
 
+module.exports = async (req, res) => {
+
+    let name = req.query.name || null;
+    let phone= req.query.phone || null;
+    let dealer=req.query.dealer || null;
+    let location=req.query.location || null;
+    let fromdate=req.query.fromdate;
+    let todate=req.query.todate;
+    let status=req.query.status;
+    let productcategory=req.query.productcategory;
+    let productname=req.query.productname;
+    let allotment=req.query.allotment;
+    let servicetype=req.query.servicetype;
+
+    // Create filter formula based on whether category is provided
+    let filterFormula = `AND(IS_AFTER({date and time of complain}, "${fromdate}"), 
+  IS_BEFORE({date and time of complain}, "${todate}"), 
+  OR(
+    SEARCH("${location}", {Location}) > 0, 
+    SEARCH("${name}", {Name}) > 0,
+    SEARCH("${dealer}", {Dealer}) > 0,
+    SEARCH("${phone}", {Phone number}) > 0,
+    SEARCH("${status}", {Status}) > 0,
+    SEARCH("${productcategory}", {product category}) > 0,
+    SEARCH("${productname}", {product name}) > 0,
+    SEARCH("${allotment}", {Allotted to}) > 0,
+    SEARCH("${servicetype}", {Service type}) > 0
+  ))` 
+  try {
+    // Perform the select query with dynamic field filtering using the Airtable API
+    const records = await base('admin').select({
+      filterByFormula: filterFormula,
+      maxRecords: 3,
+      view: "Grid view"
+    }).all();  // Use `.all()` to fetch all records based on the filter
+
+    // Process the retrieved records
+    if (records.length > 0) {
+      const retrievedRecords = records.map(record => {
+        return {
+          id: record.get('Complain Number'),
+          compcode: record.get('Complain Number'), // Change 'empcode' to the desired fields
+          Phone: record.get('Phone Number'),
+          Name: record.get('Customer Name'),
+          Dealer: record.get('Dealer'),
+          Location: record.get('Location'),
+          Status: record.get('Status'),
+          "complain type":record.get("complain type"),
+          "Date and time of complain": record.get('date and time of complain'),
+          "Product category": record.get('product category'),
+          "Product name": record.get('product name'),
+          "Allotted to": record.get('Allotted to'),
+          "Service type": record.get('Request type'),
+          
+
+          
+        
+
+
+           // Assuming you want to fetch the Phone field too
+          // Add any other fields you wish to return
+        };
+      });
+
+      res.status(200).json(retrievedRecords);  // Send the fetched records as JSON response
+    } else {
+      res.status(404).json({ message: 'No records found for that filter' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching records from Airtable');
+  }
+}
