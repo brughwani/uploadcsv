fe790f1d1f6afeba5a82c7bc22bf7c425c1da117
brughwani <brughwani@gmail.com>
2024-10-24 15:12:54 +0530

    debugging of production bugs

--- api/addemployee.js
+++ api/addemployee.js
@@ -7,7 +7,7 @@ const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.e
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
