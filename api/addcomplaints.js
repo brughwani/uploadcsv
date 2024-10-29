05ab4706aa336ae5fc13da876a6807aa8fdeaab8
brughwani <brughwani@gmail.com>
2024-10-29 16:28:26 +0530

    added credential line in addcomplaints

--- api/addcomplaints.js
+++ api/addcomplaints.js
@@ -7,6 +7,8 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
   
     if (req.method === 'OPTIONS') {
       res.status(200).end();
