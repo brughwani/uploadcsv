87f4ee3dee5a9e4e8f182af23480d5df2a885c79
brughwani <brughwani@gmail.com>
2024-11-07 11:42:02 +0530

    added dealer dropdown

---
+++ api/customer.js
@@ -0,0 +1,56 @@
+const Airtable = require('airtable');
+
+module.exports = async (req, res) => {
+  const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+  // Get category from query parameters
+  let category = req.query.category || null;
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method !== 'GET') {
+    res.status(405).send('Method Not Allowed');
+    return;
+  }
+
+  // Create filter formula based on whether category is provided
+  let filterFormula = loc ? `{locality} = "${loc}"` : '';
+
+  try {
+    const records = [];
+    base('Dealer')
+      .select({
+        filterByFormula: filterFormula, // Apply filter only if category exists
+        view: "Grid view",
+      })
+      .eachPage(
+        function page(recordBatch, fetchNextPage) {
+          recordBatch.forEach(record => {
+            records.push({
+              id: record.id,
+              dealerName: record.get('Dealer Name'),
+              location: record.get('locality'), // Get category for each product
+            });
+          });
+          fetchNextPage(); // Fetch the next page of records
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to fetch data' });
+          } else {
+            res.status(200).json(records); // Return the array of product records
+          }
+        }
+      );
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
---
+++ api/location.js
@@ -0,0 +1,51 @@
+const Airtable = require('airtable');
+
+module.exports = async (req, res) => {
+  
+  const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appoTE4TCM6nEPxJI');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method !== 'GET') {
+    res.status(405).send('Method Not Allowed');
+    return;
+  }
+  try {
+    const categories = new Set(); // Using a Set to store unique categories
+
+    base('Dealer')
+      .select({
+        view: "Grid view", // You can use other views if needed
+      })
+      .eachPage(
+        function page(recordBatch, fetchNextPage) {
+          recordBatch.forEach(record => {
+            const location = record.get('locality');
+            if (location) {
+              locations.add(location); // Add category to Set to ensure uniqueness
+            }
+          });
+          fetchNextPage(); // Fetch the next page of records
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to fetch data' });
+          } else {
+            // Convert Set to Array and return unique categories
+            res.status(200).json(Array.from(locations));
+          }
+        }
+      );
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
