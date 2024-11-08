149ab68c4f90fe29ad959fd88ee50e8d9dd8ccdd
brughwani <brughwani@gmail.com>
2024-11-08 16:58:05 +0530

    off set ko up set

--- api/location.js
+++ api/location.js
@@ -19,34 +19,41 @@ module.exports = async (req, res) => {
     return;
   }
   try {
-    const categories = new Set(); // Using a Set to store unique categories
+    const locations = new Set(); // Using a Set to store unique categories
+    let offset = null;
 
-    base('Dealer')
+const getrecords = async () => {
+    
+
+   const response = await base('Dealer')
       .select({
         view: "Grid view", // You can use other views if needed
-      })
-      .eachPage(
-        function page(recordBatch, fetchNextPage) {
-          recordBatch.forEach(record => {
+        pageSize:100,
+        offset:offset
+      }).all
+
+      response.forEach(record => {
             const location = record.get('locality');
             if (location) {
-              locations.add(location); // Add category to Set to ensure uniqueness
+              locations.add(location); // Add locations to Set to ensure uniqueness
             }
           });
 
-          fetchNextPage(); // Fetch the next page of records
-        },
-        function done(err) {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Failed to fetch data' });
-          } else {
-            // Convert Set to Array and return unique categories
-            res.status(200).json(Array.from(locations));
+          offset = response.offset; // Update offset for the next page if it exists
+          if (offset) {
+            await getRecords(); // Recursively fetch the next page
           }
-        }
-      );
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' });
-  }
-};
+          getRecords(); // Fetch the next page of records
+        };
+        await getRecords();
+
+        // Convert Set to Array and send the response
+        res.status(200).json(Array.from(locations));
+      } catch (error) {
+        console.error("Server error:", error);
+        res.status(500).json({ error: 'Server error' });
+      }
+    };
+       
+ 
+  
