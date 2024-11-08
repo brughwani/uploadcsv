3003913d7ff0097bf6a87ea717e3787409578edd
brughwani <brughwani@gmail.com>
2024-11-08 17:35:05 +0530

    offset setting

--- api/location.js
+++ api/location.js
@@ -29,31 +29,52 @@ const getRecords = async () => {
       .select({
         view: "Grid view", // You can use other views if needed
         pageSize:100,
-        offset:offset
-      }).firstPage();
-
-      response.forEach(record => {
-            const location = record.get('locality');
-            if (location) {
-              locations.add(location); // Add locations to Set to ensure uniqueness
-            }
-          });
-
-          offset = response.offset; // Update offset for the next page if it exists
-          if (offset) {
-            await getRecords(); // Recursively fetch the next page
+        ...(offset ? { offset: offset } : {})
+      }).eachPage((records, fetchNextPage) => {
+        records.forEach(record => {
+          const location = record.get('locality');
+          if (location) {
+            locations.add(location); // Add locations to Set to ensure uniqueness
           }
-          getRecords(); // Fetch the next page of records
-        };
-        await getRecords();
+        });
+         // Fetch the next page of records
+         fetchNextPage();
+        });
+      };
+  
+      // Start fetching records
+      await getRecords();
+  
+      // Convert Set to Array and send the response
+      res.status(200).json(Array.from(locations));
+    } catch (error) {
+      console.error("Server error:", error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  };
 
-        // Convert Set to Array and send the response
-        res.status(200).json(Array.from(locations));
-      } catch (error) {
-        console.error("Server error:", error);
-        res.status(500).json({ error: 'Server error' });
-      }
-    };
+    //   response.forEach(record => {
+    //         const location = record.get('locality');
+    //         if (location) {
+    //           locations.add(location); // Add locations to Set to ensure uniqueness
+    //         }
+    //       });
+
+    //       offset = response.offset; // Update offset for the next page if it exists
+    //       if (offset) {
+    //         await getRecords(); // Recursively fetch the next page
+    //       }
+    //       getRecords(); // Fetch the next page of records
+    //     };
+    //     await getRecords();
+
+    //     // Convert Set to Array and send the response
+    //     res.status(200).json(Array.from(locations));
+    //   } catch (error) {
+    //     console.error("Server error:", error);
+    //     res.status(500).json({ error: 'Server error' });
+    //   }
+    // };
        
  
   
