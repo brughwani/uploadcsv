8ccad3ae2eede1e23e9ffcdb51f16af80bb09181
brughwani <brughwani@gmail.com>
2024-12-08 09:02:59 +0530

    corsification

--- api/filtercomplaint.js
+++ api/filtercomplaint.js
@@ -104,6 +104,19 @@ const Airtable = require('airtable');
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
 
 module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method !== 'GET') {
+    res.status(405).send('Method Not Allowed');
+    return;
+  }
     // Extract query parameters
     const {
         name = null,
