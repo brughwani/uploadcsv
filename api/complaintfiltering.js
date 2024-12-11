6d5660d2b24d5fc9c42a16563e6e88ac140dc7d7
brughwani <brughwani@gmail.com>
2024-12-11 18:44:39 +0530

    facepalm moment again

--- api/complaintfiltering.js
+++ api/complaintfiltering.js
@@ -79,7 +79,7 @@ console.log(adminRecords)
        //     if (phone && record.get('Phone Number')?.toString() !== phone) matches = false
             if (productname && record.get('product name (from Serviceid)')?.toString() !== productname.toLowerCase()) matches = false
             if(location && record.get('City (from Serviceid)')?.toString() !== location.toLowerCase()) matches= false
-            if(productcategory && record.get('category (from Serviceid)')?.toString() !== category.toLowerCase()) matches= false
+            if(productcategory && record.get('category (from Serviceid)')?.toString() !== productcategory.toLowerCase()) matches= false
             if(matches)
             {
                 filteredRecords.push({
